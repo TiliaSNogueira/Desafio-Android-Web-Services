5efcd218fb3cc3ca124084d6a1d9b2f2a95d596f
TiliaSNogueira <tiliasalomao@gmail.com>
2020-12-01 12:52:59 -0300

    fragment cadastro inicio

--- app/src/main/java/com/e/marvelhqs/ui/FragmentCadastro.kt
+++ app/src/main/java/com/e/marvelhqs/ui/FragmentCadastro.kt
@@ -7,25 +7,23 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.e.marvelhqs.R
-import kotlinx.android.synthetic.main.body_login_white.view.*
+import kotlinx.android.synthetic.main.body_cadastro_white.view.*
 
-class FragmentLogin : Fragment() {
-
+class FragmentCadastro : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_login, container, false)
+        val view = inflater.inflate(R.layout.fragment_cadastro, container, false)
 
-        view.btn_login.setOnClickListener {
-            findNavController().navigate(R.id.action_fragmentLogin_to_fragmentListaHQs)
+
+        view.btn_save.setOnClickListener {
+            findNavController().navigate(R.id.action_fragmentCadastro_to_fragmentListaHQs)
         }
 
-        view.tv_create_account_frag_login.setOnClickListener {
-            findNavController().navigate(R.id.action_fragmentLogin_to_fragmentCadastro)
-        }
+
 
 
         return view
