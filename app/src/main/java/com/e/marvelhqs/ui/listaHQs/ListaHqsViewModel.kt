cb9b7011b2a4c218f6ef7dff0e97f4f6714e5e76
TiliaSNogueira <tiliasalomao@gmail.com>
2020-12-04 16:19:54 -0300

    tentando try catch

--- app/src/main/java/com/e/marvelhqs/ui/listaHQs/ListaHqsViewModel.kt
+++ app/src/main/java/com/e/marvelhqs/ui/listaHQs/ListaHqsViewModel.kt
@@ -1,5 +1,6 @@
 package com.e.marvelhqs.ui.listaHQs
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -7,15 +8,29 @@ import com.e.marvelhqs.Results
 import com.e.marvelhqs.repository.Service
 import com.e.marvelhqs.repository.service
 import kotlinx.coroutines.launch
+import retrofit2.HttpException
 
 class ListaHqsViewModel(service: Service) : ViewModel() {
 
     val listaComics = MutableLiveData<List<Results>>()
 
-    fun getListHQs(offset: Int) {
-        viewModelScope.launch {
-            listaComics.value = service.getLisHQRepo(offset).data.results
+    fun getListHQs(offset: Int) : List<Results>? {
+
+        try {
+            viewModelScope.launch {
+                listaComics.value = service.getLisHQRepo(offset).data.results
+            }
+
+            return listaComics.value
+
+
+        //ERA PRA ENTRAR NO CATH QUANDO A URL ESTÁ COM ALGUM ERRO DE DIGITAÇÃO, MAS NÃO ESTÁ INDO
+        } catch (e: HttpException){
+            Log.i("LISTAHQVIEWMODEL", e.message.toString())
+
+            return listaComics.value
         }
+
     }
 
 }
